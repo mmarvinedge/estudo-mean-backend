f0ec6dafb8b6efc1d6cfad8dd428650c079ee641
Marvin Edge <marvinedge@live.com>
2020-03-11 14:51:12 -0300

    routes validators

--- config/routes.js
+++ config/routes.js
@@ -4,5 +4,8 @@ module.exports = function(server) {
     // API Routes
     const router = express.Router()
     server.use('/api', router)
-    
+
+    // rotas da API
+    const billingCycleService = require('../api/billingCycle/billingCycleServices')
+    billingCycleService.register(router, '/billingCycles')
 }
