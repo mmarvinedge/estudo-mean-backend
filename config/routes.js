49fe3c5c26f7c5c189594ebbd2f8d774c5c5363e
Marvin Edge <marvinedge@live.com>
2020-03-11 17:00:57 -0300

    include summary for credit and debt

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 module.exports = function(server) {
+
     // API Routes
     const router = express.Router()
     server.use('/api', router)
@@ -8,4 +9,7 @@ module.exports = function(server) {
     // rotas da API
     const billingCycleService = require('../api/billingCycle/billingCycleServices')
     billingCycleService.register(router, '/billingCycles')
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+    router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
